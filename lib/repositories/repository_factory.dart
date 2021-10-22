c58cfb81cf610d7f4c7feddb5a29762077040163
nabil lakrib <pni20156789@gmail.com>
2021-10-22 14:36:07 +0100

    check connectivity

--- lib/repositories/repository_factory.dart
+++ lib/repositories/repository_factory.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flashcards/cubits/sync_cubit.dart';
 import 'package:flashcards/models/cached_sync_dates.dart';
 import 'package:flashcards/models/stats.dart';
@@ -9,14 +10,23 @@ import 'local_repository.dart';
 import 'provider.dart';
 
 class ReposityFactory extends Provider {
+  late Future<bool> isOnline;
   final RemoteRepository _remote;
   final LocalRepository _local;
   SyncCubit? _sync;
+
   ReposityFactory(this._remote, this._local) {
+    isOnline = Connectivity().checkConnectivity().then((value) {
+      return value != ConnectivityResult.none;
+    });
+
+    // todo add connectivity listener;
   }
 
   @override
   Future<List<Cart>> getCards() async {
+    await isOnline;
+    // TODO: implement getCards
     throw UnimplementedError();
   }
 
@@ -30,16 +40,20 @@ class ReposityFactory extends Provider {
 
   @override
   Future<void> submitScore(Score score) async {
+    await isOnline;
+
     // TODO: implement submitScore
     throw UnimplementedError();
   }
 
   @override
   Future<dynamic> getLatestUpdates(CachedSyncDates dates) async {
+    print(await isOnline);
   }
 
   @override
   Future<void> updateSpecialCard(String id, bool boosted) async {
+    print(await isOnline);
   }
 
   void hookSync(SyncCubit sync) {
